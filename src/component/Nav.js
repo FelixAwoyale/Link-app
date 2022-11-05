9216c39147d9d7d31b81321208d79bb806d303e1
FelixYale <dexteryale@gmail.com>
2022-11-06 00:32:34 +0100

    feat:other changes

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -3,7 +3,11 @@ import Avatar from "../assets/Images/avatar.png";
 
 export default function Nav() {
   return (
-    <div>
+      <div>
+          <div>
+              
+          </div>
+
       <div className="nav-section">
         <img src={Avatar} alt="avatar" id="profile__img" />
       </div>
